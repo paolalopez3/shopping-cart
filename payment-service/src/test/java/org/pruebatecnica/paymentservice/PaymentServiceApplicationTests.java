595211d043cf8a1a681f84b4448552c49d9a094a
paolalopez3 <lopezpaolaa23@uca.edu.sv>
2026-05-09 16:05:23 -0600

    chore: init

---
+++ payment-service/src/test/java/org/pruebatecnica/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pruebatecnica.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
