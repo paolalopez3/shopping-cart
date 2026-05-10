b4677a0d85158501c5bf0d827ba06c8c85bc4c3e
paolalopez3 <lopezpaolaa23@gmail.com>
2026-05-10 09:19:55 -0600

    feat: add order repository

---
+++ order-service/src/main/java/org/pruebatecnica/orderservice/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package org.pruebatecnica.orderservice.repository;
+
+import org.pruebatecnica.orderservice.entity.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, UUID> {
+}
