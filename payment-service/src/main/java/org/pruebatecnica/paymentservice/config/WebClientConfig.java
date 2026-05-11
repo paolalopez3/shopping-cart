a2d46d676e64c339d7274dfb505991faa7d38419
paolalopez3 <lopezpaolaa23@gmail.com>
2026-05-11 01:15:25 -0600

    feat: add webclient to have the centralized url of the order-service microservice

---
+++ payment-service/src/main/java/org/pruebatecnica/paymentservice/config/WebClientConfig.java
@@ -0,0 +1,17 @@
+package org.pruebatecnica.paymentservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+    @Value("${order-service.base-url}")
+    private String baseUrl;
+
+    public WebClient webClient() {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .build();
+    }
+}
