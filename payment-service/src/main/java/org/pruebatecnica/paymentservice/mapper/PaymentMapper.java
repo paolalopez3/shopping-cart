7d463fcec503c4a5452d60956ee8f9c777d6cb24
paolalopez3 <lopezpaolaa23@gmail.com>
2026-05-11 01:07:10 -0600

    feat: add payment mapper

---
+++ payment-service/src/main/java/org/pruebatecnica/paymentservice/mapper/PaymentMapper.java
@@ -0,0 +1,28 @@
+package org.pruebatecnica.paymentservice.mapper;
+
+import org.pruebatecnica.paymentservice.dto.request.PaymentRequestDto;
+import org.pruebatecnica.paymentservice.dto.response.PaymentResponseDto;
+import org.pruebatecnica.paymentservice.entity.Payment;
+import org.springframework.stereotype.Component;
+
+import java.util.UUID;
+
+@Component
+public class PaymentMapper {
+    public Payment toPayment(PaymentRequestDto paymentRequestDto) {
+        return Payment.builder()
+                .orderId(paymentRequestDto.getOrderId())
+                .build();
+    }
+
+    public PaymentResponseDto toPaymentResponseDto(Payment payment) {
+        return PaymentResponseDto.builder()
+                .id(payment.getId())
+                .orderId(payment.getOrderId())
+                .totalAmount(payment.getTotalAmount())
+                .paymentStatus(payment.getPaymentStatus())
+                .paidAt(payment.getPaidAt())
+                .updatedAt(payment.getUpdatedAt())
+                .build();
+    }
+}
