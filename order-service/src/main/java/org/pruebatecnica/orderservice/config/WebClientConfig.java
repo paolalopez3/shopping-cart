0d83909085534b106a6f7fc87d91da3288b7b6cf
paolalopez3 <lopezpaolaa23@gmail.com>
2026-05-10 23:52:36 -0600

    fix: add missing parameter to get base-url from .yaml in webClientConfig

--- order-service/src/main/java/org/pruebatecnica/orderservice/config/WebClientConfig.java
+++ order-service/src/main/java/org/pruebatecnica/orderservice/config/WebClientConfig.java
@@ -1,5 +1,6 @@
 package org.pruebatecnica.orderservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -10,7 +11,7 @@ public class WebClientConfig {
     private String baseUrl;
 
     @Bean
-    public WebClient webClient() {
+    public WebClient webClient(@Value("${product-service.base-url}") String baseUrl) {
         return WebClient.builder()
                 .baseUrl(baseUrl)
                 .build();
