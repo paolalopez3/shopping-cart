999c2c57c64d7358ebdf944b47d82b9de3f6b613
paolalopez3 <lopezpaolaa23@gmail.com>
2026-05-11 01:37:17 -0600

    feat: added payment service implementation

--- payment-service/src/main/java/org/pruebatecnica/paymentservice/repository/PaymentRepository.java
+++ payment-service/src/main/java/org/pruebatecnica/paymentservice/repository/PaymentRepository.java
@@ -2,8 +2,10 @@ package org.pruebatecnica.paymentservice.repository;
 
 import org.pruebatecnica.paymentservice.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 }
---
+++ payment-service/src/main/java/org/pruebatecnica/paymentservice/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,42 @@
+package org.pruebatecnica.paymentservice.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.pruebatecnica.paymentservice.dto.request.PaymentRequestDto;
+import org.pruebatecnica.paymentservice.dto.response.PaymentResponseDto;
+import org.pruebatecnica.paymentservice.entity.Payment;
+import org.pruebatecnica.paymentservice.entity.enums.PaymentStatus;
+import org.pruebatecnica.paymentservice.mapper.PaymentMapper;
+import org.pruebatecnica.paymentservice.repository.PaymentRepository;
+import org.pruebatecnica.paymentservice.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService {
+    private final PaymentRepository paymentRepository;
+    private final PaymentMapper paymentMapper;
+
+    @Override
+    public PaymentResponseDto makePayment(PaymentRequestDto paymentRequestDto) {
+        Payment payment = Payment.builder()
+                .orderId(paymentRequestDto.getOrderId())
+                .paymentStatus(PaymentStatus.PENDING)
+                .build();
+
+        if (Math.random() < 0.8) {
+            payment.setPaymentStatus(PaymentStatus.APPROVED);
+        } else {
+            payment.setPaymentStatus(PaymentStatus.REJECTED);
+        }
+
+        return paymentMapper.toPaymentResponseDto(paymentRepository.save(payment));
+    }
+
+    @Override
+    public PaymentResponseDto getPaymentById(UUID id) {
+        Payment payment = paymentRepository.findById(id).orElse(null);
+        return paymentMapper.toPaymentResponseDto(payment);
+    }
+}
