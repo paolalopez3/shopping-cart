30c3107c9fb11d10a03620d80e6545a1ce1dbcc3
paolalopez3 <lopezpaolaa23@gmail.com>
2026-05-11 01:41:49 -0600

    feat: added personalized exception adn global exception handler

---
+++ payment-service/src/main/java/org/pruebatecnica/paymentservice/exception/PaymentNotFoundException.java
@@ -0,0 +1,7 @@
+package org.pruebatecnica.paymentservice.exception;
+
+public class PaymentNotFoundException extends RuntimeException {
+    public PaymentNotFoundException(String message) {
+        super(message);
+    }
+}
