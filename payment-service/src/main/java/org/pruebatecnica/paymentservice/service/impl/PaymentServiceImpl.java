923b348fdd529462768b26fac43c2020cd6b42ae
paolalopez3 <lopezpaolaa23@gmail.com>
2026-05-11 02:00:36 -0600

    fix: resolve issue with totalamount being 0.0 by using the missing client in service

--- payment-service/src/main/java/org/pruebatecnica/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/org/pruebatecnica/paymentservice/service/impl/PaymentServiceImpl.java
@@ -1,6 +1,7 @@
 package org.pruebatecnica.paymentservice.service.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.pruebatecnica.paymentservice.client.OrderClient;
 import org.pruebatecnica.paymentservice.dto.request.PaymentRequestDto;
 import org.pruebatecnica.paymentservice.dto.response.PaymentResponseDto;
 import org.pruebatecnica.paymentservice.entity.Payment;
@@ -18,12 +19,14 @@ import java.util.UUID;
 public class PaymentServiceImpl implements PaymentService {
     private final PaymentRepository paymentRepository;
     private final PaymentMapper paymentMapper;
+    private final OrderClient orderClient;
 
     @Override
     public PaymentResponseDto makePayment(PaymentRequestDto paymentRequestDto) {
         Payment payment = Payment.builder()
                 .orderId(paymentRequestDto.getOrderId())
                 .paymentStatus(PaymentStatus.PENDING)
+                .totalAmount(orderClient.getOrderById(paymentRequestDto.getOrderId()).getTotalAmount())
                 .build();
 
         if (Math.random() < 0.8) {
